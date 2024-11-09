7a979758e050c96aaa3cd7c766c2c95b504f15eb
cyb3rfortr3ss <gospelchukwunonso@proton.me>
2024-11-09 02:33:51 -1000

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,14 @@
+clear
+echo "Starting installation..."
+echo
+pip3 install colorama
+pip3 install art
+pip3 install requests
+
+chmod +x jsRecon
+cp jsRecon /usr/bin
+
+clear
+echo "Setup Completed... You can now run ./jsRecon from any environment"
+echo
+
